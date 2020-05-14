5aea5a3ad7898b5f1add76899c776c87a8943cd0
lkl089 <kimjunghyeon5801@gmail.com>
2020-05-15 00:55:31 +0900

    Update checkurl.py

--- data/checkurl.py
+++ data/checkurl.py
@@ -2,7 +2,7 @@
 import urllib.request
 import urllib.error
 from datetime import datetime,date,timedelta
-
+from data import recent_confirmed as week
 
 
 #driver = webdriver.Chrome('c:/chromedriver/chromedriver.exe')
@@ -17,49 +17,21 @@ month = now.strftime("%m")
 day = now.strftime("%d")
 today = month + day
 
-url1 = "http://ncov.mohw.go.kr/static/image/main_chart/live_pdata1_" + today + ".png"
-url2 = "http://ncov.mohw.go.kr/static/image/main_chart/live_pdata2_" + today + ".png"
-#url3 = ""
-try:
-    res = urllib.request.urlopen(url1)
-    print(res.status)
-    print("URL1 is alive")
+
+now = datetime.now()
+month = now.strftime("%m")
+day = now.strftime("%d")
+today = month + '.' + day
+print('here!!')
+print(today)
+print(week.today)
+if today == week.today:
     update = month+"/"+day+" 기준"
-except urllib.error.HTTPError as e:
-    err = e.read()
-    code = e.getcode()
-    broke = "URL1 is broken"
-    print(code)  ## 404
-    print(broke)
+    print(update)
+else:
     today = date.today() - timedelta(1)
     month = today.strftime("%m")
     day = today.strftime("%d")
     today = month + day
-    print(today)
     update = month + "/" + day + " 기준"
-
-try:
-    res = urllib.request.urlopen(url2)
-    print(res.status)
-    print("URL2 is alive")
-    update = month + "/" + day + " 기준"
-except urllib.error.HTTPError as e:
-    err = e.read()
-    code = e.getcode()
-    broke="URL2 is broken"
-    print(code)  ## 404
-    print(broke)
-    today = date.today() - timedelta(1)
-    month = today.strftime("%m")
-    day = today.strftime("%d")
-    today = month + day
-    print(today)
-    update = month + "/" + day + " 기준"
-
-#try:
-#    res = urllib.request.urlopen(url3)
-#    print(res.status)
-#except urllib.error.HTTPError as e:
-#    err = e.read()
-#    code = e.getcode()
-#    print(code+"URL1 is broken")  ## 404
+    print(update)
